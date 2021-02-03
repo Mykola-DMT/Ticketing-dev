18004fec0a31408ab58ee8ae365ca3302c9dff9e
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-02-03 08:27:30 +0200

    master

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import {json} from 'body-parser'
-import {currentUserRouter} from './routes/current-user'
+import {currentUserRouter} from './routes/current-user' 
 import {signinRouter} from './routes/signin'
 import {signoutRouter} from './routes/signout'
 import {signupRouter} from './routes/signup'
