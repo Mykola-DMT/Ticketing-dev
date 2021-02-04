ac8e855d017e693e87b71bb24335c3f41abb721d
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-02-05 00:11:00 +0200

    master

---
+++ auth/src/models/user.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+import { Password } from '../services/password'
+
+//An interface that describe the properties
+//that are required to create a new User
+interface UserAttrs{
+    email: string
+    password: string
+}
+
+//An interface that desribes the properites 
+//that a User model has 
+interface UserModel extends mongoose.Model<any> {
+    build(attrs: UserAttrs): UserDoc
+}
+
+//An interface that describes the properties 
+//that a User Document has
+interface UserDoc extends mongoose.Document {
+    email:string
+    password: string
+}
+
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+})
+userSchema.pre('save', async function(done){
+    if(this.isModified('password')){
+        const hashed = await Password.toHash(this.get('password'))
+        this.set('password', hashed)
+    }
+    done()
+})
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs)
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
+
+const user = User.build({
+    email:'test@test.com',
+    password:'password'
+})
+
+
+export { User }
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from 'express'
 import {body, validationResult} from 'express-validator'
+import {User} from '../models/user'
+import {BadRequestError} from '../errors/bad-request-error'
 import {DatabaseConnectionError} from '../errors/database-connection-error'
 import {RequstValidayionError} from '../errors/request-validation-error'
 
@@ -13,7 +15,7 @@ router.post('/api/users/signup', [
         .isLength({ min :4, max: 20 })
         .withMessage('Password between 4 and 20 characters')
 ],
-(req:Request, res: Response) => {
+async(req:Request, res: Response) => {
     const errors = validationResult(req)
 
     if(!errors.isEmpty()){
@@ -22,10 +24,16 @@ router.post('/api/users/signup', [
 
     const {email, password} = req.body
 
-    console.log('Creating a user');
-    throw new DatabaseConnectionError()
-
-    res.send({})
+    const existingUser = await User.findOne({email})
+    
+    if(existingUser){
+        throw new BadRequestError('Email in use')
+    }
+    const user = User.build({
+        email, password
+    })
+    await user.save()
+    res.status(201).send(user)
 })
 
 export { router as signupRouter }
