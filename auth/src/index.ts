fa8bb17cdb5dbba3b5e7ab04b004605f08e23c0d
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-02-04 08:32:11 +0200

    master

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import {json} from 'body-parser'
+import mongoose from 'mongoose'
 
 import {currentUserRouter} from './routes/current-user' 
 import {signinRouter} from './routes/signin'
@@ -22,7 +23,22 @@ app.all('*', async () => {
 })
 app.use(errorHandler)
 
+const start = async () => {
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('Connected to mongoDb');
+        
+    } catch(err){
+        console.log(err)
+    }
 
-app.listen(3000, () => {
-    console.log('Listening(Auth) on 3000...');
-})
+    app.listen(3000, () => {
+        console.log('Listening(Auth) on 3000...');
+    })
+}
+
+start()
