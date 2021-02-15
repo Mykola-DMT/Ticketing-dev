0e27d7c053d1d970afc6507fd46f9bc47c00d291
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-02-15 11:27:32 +0200

    master

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -44,10 +44,58 @@ it('return a 401 if the user does not own the ticket', async () => {
         .expect(401)
 })
 
-it('return a 400 if the user provided an invslid title or price', async () => {
+it('return a 400 if the user provided an invalid title or price', async () => {
+    const cookie = global.signin()
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'hfjdskla',
+            price: 21
+        })
 
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: '',
+            price: 21
+        })
+        .expect(400)
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'waels',
+            price: -21
+        })
+        .expect(400)
 })
 
 it('updates the ticket provided valid inputs', async () => {
+    const cookie = global.signin()
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'hfjdskla',
+            price: 21
+        })
 
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'new title',
+            price: 300
+        })
+        .expect(200)
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+
+    expect(ticketResponse.body.title).toEqual('new title')
+    expect(ticketResponse.body.price).toEqual(300)
 })
