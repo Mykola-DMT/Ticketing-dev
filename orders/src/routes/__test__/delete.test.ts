0f06bb1acc6b3ee98c96fe680658e110a06de9b5
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-03-13 18:24:55 +0200

    add orders event publishers

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { app } from '../../app'
 import { Ticket } from '../../models/ticket'
 import { Order, OrderStatus } from '../../models/order'
+import { natsWrapper } from '../../nats-wrapper'
 
 it('marksan order as cencelled', async () => {
     //create a ticket 
@@ -32,4 +33,27 @@ it('marksan order as cencelled', async () => {
     expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
 })
 
-it.todo('emits a order cancelled event')
+it('emits a order cancelled event', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 10
+    })
+    await ticket.save()
+
+    const user = global.signin()
+    //make a request to create an order
+    const { body: order } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({ ticketId: ticket.id })
+        .expect(201)
+
+    //make a request to cancell the order
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(204)
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
