732a10d111c776600abd49f109285c5a72e5ec7e
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-04-22 08:28:19 +0300

    master

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
-import express, {Request, Response} from 'express'
-import {body} from 'express-validator'
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
 import jwt from 'jsonwebtoken'
-import {User} from '../models/user'
+import { User } from '../models/user'
 
-import {BadRequestError, validationRequest} from '@mdticketss/common'
+import { BadRequestError, validationRequest } from '@mdticketss/common'
 
 const router = express.Router()
 
@@ -12,41 +12,41 @@ router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password')
         .trim()
-        .isLength({ min :4, max: 20 })
+        .isLength({ min: 4, max: 20 })
         .withMessage('Password between 4 and 20 characters')
 ],
-validationRequest,
-async(req:Request, res: Response) => {
-    // const errors = validationResult(req)
+    validationRequest,
+    async (req: Request, res: Response) => {
+        // const errors = validationResult(req)
 
-    // if(!errors.isEmpty()){
-    //     throw new RequstValidayionError(errors.array())
-    // }
+        // if(!errors.isEmpty()){
+        //     throw new RequstValidayionError(errors.array())
+        // }
 
-    const {email, password} = req.body
+        const { email, password } = req.body
 
-    const existingUser = await User.findOne({email})
-    
-    if(existingUser){
-        throw new BadRequestError('Email in use')
-    }
-    const user = User.build({
-        email, password
+        const existingUser = await User.findOne({ email })
+
+        if (existingUser) {
+            throw new BadRequestError('Email in use')
+        }
+        const user = User.build({
+            email, password
+        })
+        await user.save()
+
+        //Generate JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.jwt_key!)
+
+        //Store on session object
+        req.session = {
+            jwt: userJwt
+        }
+
+        res.status(201).send(user)
     })
-    await user.save()
-
-    //Generate JWT
-    const userJwt = jwt.sign({
-        id: user.id,
-        email: user.email
-    }, process.env.jwt_key!)
-
-    //Store on session object
-    req.session = {
-        jwt: userJwt
-    }
-
-    res.status(201).send(user)
-})
 
 export { router as signupRouter }
