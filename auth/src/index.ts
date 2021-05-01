b79663356c548afd9d06f8f87f2c28052bee833e
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-05-01 22:36:00 +0300

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,23 +1,24 @@
 import mongoose from 'mongoose'
-import {app } from './app'
+import { app } from './app'
 
 const start = async () => {
-    if(!process.env.jwt_key){
+    console.log('Starting up...')
+    if (!process.env.jwt_key) {
         throw new Error('JWT must be defined')
     }
-    if(!process.env.MONGO_URI){
+    if (!process.env.MONGO_URI) {
         throw new Error('Mongo_URI must be defined')
     }
 
-    try{
+    try {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
         })
         console.log('Connected to mongoDb');
-        
-    } catch(err){
+
+    } catch (err) {
         console.log(err)
     }
 
