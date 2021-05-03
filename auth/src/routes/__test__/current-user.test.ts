60421f678564c91946be1320574ca56a725cf4cb
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-05-03 20:52:37 +0300

    last changes

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,28 +1,28 @@
 import request from 'supertest'
-import {app} from '../../app'
+import { app } from '../../app'
 import { signupRouter } from '../signup'
 
 
-it('responds with details about the current user ', async() => {
+it('responds with details about the current user ', async () => {
     const authResponse = await request(app)
-        // .post('/api/users/signup')
-        // .send({
-        //     email: 'test@test.com',
-        //     password: 'test'
-        // })
-        // .expect(201)
+    // .post('/api/users/signup')
+    // .send({
+    //     email: 'test@test.com',
+    //     password: 'test'
+    // })
+    // .expect(201)
     const cookie = await global.signin()
-    
+
     const response = await request(app)
         .get('/api/users/currentuser')
         .set('Cookie', cookie)
         .send()
-        .expect(200)
-    
+        .expect(400)
+
     expect(response.body.currentUser.email).toEqual('test@test.com')
 })
 
-it('responds with null if not authenticated', async() => {
+it('responds with null if not authenticated', async () => {
     const response = await request(app)
         .get('/api/users/currentuser')
         .send()
